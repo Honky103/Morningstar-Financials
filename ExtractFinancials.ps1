4d1f839212756404e7006f2b11d7cc01536291d8
Honky103 <pang.hong.ming@gmail.com>
2019-06-19 15:59:39 +0800

    New Branch - GIM_calculator v0.0
    New branch to check extracted financial information against GIM requirements

--- ExtractFinancials.ps1
+++ ExtractFinancials.ps1
@@ -21,7 +21,7 @@
 $ie = New-Object -com internetexplorer.application;
 $ie.visible = $false;
 $tabName = 'Financials'
-$financial_table = New-Object system.Data.DataTable Ã¢â‚¬Å“$tabNameÃ¢â‚¬Â
+$financial_table = New-Object system.Data.DataTable â€œ$tabNameâ€
 
 $gim_score = 0;
 $year = $null
@@ -147,7 +147,7 @@ for($j=0;$j -lt 5; $j++)
           $temp = $temp -replace '[()]',"";
           $temp = 0-[double]$temp;  
         } 
-        if ($temp -eq 'â€”')
+        if ($temp -eq '—')
         {
             $temp = 0;
         }
@@ -199,7 +199,7 @@ for($j=0;$j -lt 6; $j++)
           $temp = $temp -replace '[()]',"";
           $temp = 0-[double]$temp;  
         } 
-        if ($temp -eq 'â€”')
+        if ($temp -eq '—')
         {
             $temp = 0;
         }
@@ -249,7 +249,7 @@ for($j=0;$j -lt 3; $j++)
           $temp = $temp -replace '[()]',"";
           $temp = 0-[double]$temp;  
         }
-        if ($temp -eq 'â€”')
+        if ($temp -eq '—')
         {
             $temp = 0;
         }
@@ -266,6 +266,165 @@ $ie.quit()
 #Displays data in the console for quick view
 $financial_table | format-table -AutoSize
 
+Write-Host "************************Checking with GIM Requirements (IMPORTANT!!!)************************" -ForegroundColor Red
+
+########## Now it is time to check against GIM's requirements ##########
+#1. Operating Profit Margin more than 8%                  (Score: 0.5)
+#2. EBIT Growth (CAGR) more than 10% for the past 2 years (Score: 1)
+#3. Net Debt to Equity less than 40%                      (Score: 1.5)
+#4. Return of Equity (ROE) greater than 15%               (Score: 1.5)
+#5. Quality of Earnings (QoE) greater than 80%            (Score: 1)
+#6. Vampire Ratio (CAPEX/OCF) less than 50%               (Score: 1)
+#7. Cash Conversion Cycle (CCC) less than 130 days        (Score: 1)
+#   Total Score                                           (Score: 7.5)
+
+$gim_table = New-Object system.Data.DataTable "$tabName";
+$gim_table.Columns.Add("Criteria")| out-null;
+$gim_table.Columns.Add("Value")| out-null;
+$gim_table.Columns.Add("Score")| out-null;
+
+
+#1. Operating Profit Margin Check
+$OPM = [math]::Round(($financial_table.($year[5])[3]/$financial_table.($year[5])[0])*100,2);
+if ($OPM -ge 8)
+{
+    $score = 0.5;
+} 
+else
+{ 
+    $score = 0;
+} 
+
+$rowcriteria = "Operating Profit Margin more than 8%";
+$rowvalue = [string]($OPM) + '%';
+$rowScore = [string]($score) + "/0.5";
+$gim_table.Rows.Add($rowcriteria,$rowvalue,$rowscore)| out-null;
+$gim_score += $score;
+
+#2. EBIT Growth Check
+$EBIT_growth = [math]::Round(([math]::sqrt($financial_table.($year[5])[3]/$financial_table.($year[3])[3])-1)*100,2);
+if ([double]$financial_table.($year[5])[3] -lt 0)
+#check for negative growth issues, when dividing negative with negative, it cancels out and result in false positive EBIT growth
+{
+    $EBIT_growth = 0-$EBIT_growth;
+}
+if ($EBIT_growth -ge 10) 
+{
+    $score = 1;
+} 
+else
+{ 
+    $score = 0;
+} 
+
+$rowcriteria = "EBIT Growth (CAGR) more than 10% for the past 2 years";
+$rowvalue = [string]($EBIT_growth) + '%';
+$rowScore = [string]($score) + "/1";
+$gim_table.Rows.Add($rowcriteria,$rowvalue,$rowscore)| out-null;
+$gim_score += $score;
+
+#3. Net Debt to Equity Check
+$gear_ratio = [math]::Round((($financial_table.($year[4])[9]-$financial_table.($year[4])[8])/$financial_table.($year[4])[10])*100,2);
+if ($gear_ratio -ge 40)
+{
+    $score = 0;
+} 
+else
+{ 
+    $score = 1.5;
+} 
+
+$rowcriteria = "Net Debt to Equity less than 40%";
+$rowvalue = [string]($gear_ratio) + '%';
+$rowScore = [string]($score) + "/1.5";
+$gim_table.Rows.Add($rowcriteria,$rowvalue,$rowscore)| out-null;
+$gim_score += $score;
+
+#4. Return of Equity Check
+$roe = [math]::Round(($financial_table.($year[5])[3]/$financial_table.($year[4])[10])*100,2);
+if ($roe -ge 15)
+{
+    $score = 1.5;
+} 
+else
+{ 
+    $score = 0;
+} 
+
+$rowcriteria = "Return of Equity (ROE) greater than 15%";
+$rowvalue = [string]($roe) + '%';
+$rowScore = [string]($score) + "/1.5";
+$gim_table.Rows.Add($rowcriteria,$rowvalue,$rowscore)| out-null;
+$gim_score += $score;
+
+#5. Quality of Earnings Check
+$qoe = [math]::Round(($financial_table.($year[5])[11]/$financial_table.($year[5])[3])*100,2);
+if ([double]$financial_table.($year[5])[3] -lt 0)
+#checks for negative operating income, which results in income loss
+{
+    $qoe = 0-$qoe;
+}
+if ($qoe -ge 80)
+{
+    $score = 1;
+} 
+else
+{ 
+    $score = 0;
+} 
+
+$rowcriteria = "Quality of Earnings (QoE) greater than 80%";
+$rowvalue = [string]($qoe) + '%';
+$rowScore = [string]($score) + "/1";
+$gim_table.Rows.Add($rowcriteria,$rowvalue,$rowscore)| out-null;
+$gim_score += $score;
+
+#6. Vampire Ratio Check
+$vampire_ratio = [math]::Round((0-$financial_table.($year[5])[12])/$financial_table.($year[5])[11]*100,2);
+if ($vampire_ratio -ge 50 -or $vampire_ratio -lt 0)
+{
+    $score = 0;
+} 
+else
+{ 
+    $score = 1;
+} 
+
+$rowcriteria = "Vampire Ratio (CAPEX/OCF) less than 50%";
+$rowvalue = [string]($vampire_ratio) + '%;';
+$rowScore = [string]($score) + "/1";
+$gim_table.Rows.Add($rowcriteria,$rowvalue,$rowscore) | out-null;
+$gim_score += $score;
+
+#7. Cash Conversion Cycle Check
+$rec = [math]::Round($financial_table.($year[4])[5]/$financial_table.($year[4])[0]*365,2);
+$pay = [math]::Round($financial_table.($year[4])[6]/$financial_table.($year[4])[1]*365,2);
+$inv = [math]::Round($financial_table.($year[4])[7]/$financial_table.($year[4])[1]*365,2);
+$ccc = $rec + $inv - $pay;
+if ($ccc -lt 130)
+{
+    $score = 1;
+} 
+else
+{ 
+    $score = 0;
+} 
+
+$rowcriteria = "Cash Conversion Cycle (CCC) less than 130 days";
+$rowvalue = [string]($ccc) + 'days';
+$rowScore = [string]($score) + "/1";
+$gim_table.Rows.Add($rowcriteria,$rowvalue,$rowscore) | out-null;
+$gim_score += $score;
+
+#Tabulates final score
+$rowcriteria = "Total score";
+$rowvalue = '-';
+$rowScore = [string]($gim_score) + "/7.5";
+$gim_table.Rows.Add($rowcriteria,$rowvalue,$rowscore) | out-null;
+
+#Displays data in the console for quick view
+$gim_table | format-table -AutoSize
+
 $pwd = get-location;
 $savefiledirectory = $pwd.Path + "\" + $stc;
 if (!(Test-Path $savefiledirectory))
@@ -278,7 +437,13 @@ Write-Host -NoNewLine "Saving financial file........................"
 $savefilename = $stc + "\Financials_"+$stc+".csv"
 $financial_table |Export-csv $savefilename
 Write-Host "Done"
-Write-Host "Your file is saved at" $savefiledirectory -ForegroundColor Green
+
+#Saves GIM table
+Write-Host -NoNewLine "Saving GIM evaluation........................"
+$savefilename = $stc + "\GIM_"+$stc+".csv"
+$gim_table |Export-csv $savefilename
+Write-Host "Done"
+Write-Host "Your files are saved at" $savefiledirectory -ForegroundColor Green
 
 Read-Host "Press Enter to exit"
 exit
